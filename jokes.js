f9e00de1cadc8d3c4addd9954988169f3d592b5d
JEROME Emilien <jerome.emi@onja.org>
2020-08-28 14:05:16 +0300

    added dad jokes exercise

--- jokes.js
+++ jokes.js
@@ -1,3 +1,7 @@
+
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+
 const buttonText = [
 	'Ugh.',
 	'🤦🏻‍♂️',
@@ -8,3 +12,34 @@ const buttonText = [
 	'please stop',
 	'that was the worst one',
 ];
+
+function randomItemArray(arr, not) {
+	const item = arr[Math.floor(Math.random() * arr.length)];
+	if(item === not) {
+		return randomItemArray(arr, not);
+	}
+	return item;
+}
+
+async function fetchJoke() {
+	const response = await fetch('https://icanhazdadjoke.com', {
+		headers: {
+			Accept: 'application/json',
+		},
+	});
+	const data = await response.json();
+	console.log(data);
+	return data;
+}
+
+async function handleClick() {
+	const {joke} = await fetchJoke();
+	console.log(joke);
+	jokeHolder.textContent = joke;
+
+	jokeButton.textContent = randomItemArray(buttonText,
+		jokeButton.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
+fetchJoke();
