710951a32a3f8b790b5d05da69d691b24c83c1aa
JEROME Emilien <jerome.emi@onja.org>
2020-09-01 16:33:03 +0300

    added dad jokes ajax refactor

--- jokes.js
+++ jokes.js
@@ -1,45 +1,6 @@
 
-const jokeButton = document.querySelector('.getJoke');
-const jokeHolder = document.querySelector('.joke p');
-
-const buttonText = [
-	'Ugh.',
-	'🤦🏻‍♂️',
-	'omg dad.',
-	'you are the worst',
-	'seriously',
-	'stop it.',
-	'please stop',
-	'that was the worst one',
-];
-
-function randomItemArray(arr, not) {
-	const item = arr[Math.floor(Math.random() * arr.length)];
-	if(item === not) {
-		return randomItemArray(arr, not);
-	}
-	return item;
-}
-
-async function fetchJoke() {
-	const response = await fetch('https://icanhazdadjoke.com', {
-		headers: {
-			Accept: 'application/json',
-		},
-	});
-	const data = await response.json();
-	console.log(data);
-	return data;
-}
-
-async function handleClick() {
-	const {joke} = await fetchJoke();
-	console.log(joke);
-	jokeHolder.textContent = joke;
-
-	jokeButton.textContent = randomItemArray(buttonText,
-		jokeButton.textContent);
-}
+import { handleClick } from './lib/handlers.js';
+import { jokeButton } from './lib/elements.js';
 
 jokeButton.addEventListener('click', handleClick);
-fetchJoke();
+// fetchJoke();
